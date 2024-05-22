3f38eb54a1c60ef7dfbbebc219a26382583001e9
Sanchit joshi <132713080+Sanchit-Joshi@users.noreply.github.com>
2024-05-23 02:21:43 +0530

    functions added

--- Basic Projects/car price prediction/static/script.js
+++ Basic Projects/car price prediction/static/script.js
@@ -1,13 +1,17 @@
 const submitbtn = document.querySelectorAll('submitbtn');
 
-
-
-
-
-
-function update() {
-
+function load_car_model(value ,  car_model){
+    let compaies = document.getElementById('value.value')
+    let carModel = document.getElementById('car_model')
+     
 }
+
+
+
+
+
+
+ 
 
 function submit() {
     submitbtn.addEventListener('click', () => { 
