ccc598d7073a1f88745c5129e3cb2d23cde8cd33
Sanchit joshi <132713080+Sanchit-Joshi@users.noreply.github.com>
2024-05-23 01:51:14 +0530

    new files added and new folder created

---
+++ Basic Projects/car price prediction/application.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+import pandas as pd
+
+car = pd.read_csv("cleaned_car_data.csv")
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    companies = sorted(car['company'].unique())
+    car_models = sorted(car['name'].unique())
+    year = sorted(car['year'].unique(),reverse=True)
+    fuel_type = car['fuel_type'].unique()
+
+    return render_template('index.html' , companies = companies, car_models = car_models, years = year, fuel_types = fuel_type)
+
+
+if __name__ == '__main__':
+    app.run(debug= True)
+
---
+++ Basic Projects/car price prediction/static/script.js
@@ -0,0 +1,16 @@
+const submitbtn = document.querySelectorAll('submitbtn');
+
+
+
+
+
+
+function update() {
+
+}
+
+function submit() {
+    submitbtn.addEventListener('click', () => { 
+        console.log('clicked');
+    });
+}
